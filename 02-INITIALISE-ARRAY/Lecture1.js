2b81f55a5e3f62387f5fb8414caddf6cbd30d6a7
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-08 13:00:10 +0600

    first commit

---
+++ 02-INITIALISE-ARRAY/Lecture1.js
@@ -0,0 +1,40 @@
+/*
+ * @Title: Initialize Array - Literal.
+ */
+
+// Array Literal:
+const arr = [];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[99] = 100;
+console.log(arr.length);
+console.log(arr);
+
+/* 
+
+length = LastIndex + 1
+LastIndex = length - 1
+
+*/
+
+const names = [
+  `Mehedi Hasan Himel`,
+  `Rayan Rakib`,
+  `Sahinur Islam`,
+  `Asifur Rahman Asif`,
+   `Rajibur Rahman Rajib`,
+  `Bipul Joy Bangla`
+];
+
+
+// names[5] = `Abdullah Kanon`
+// names[names.length] = `Abdullah Kanon`;
+
+
+console.log(names.length);
+console.log(names);
+
+// Get last Index
+
+// Calculated Index
